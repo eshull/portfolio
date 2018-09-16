7be784b3569f8cc607a7919394a0b527b0ded9c7
Eric Shull <eshulldesign@gmail.com>
2018-09-16 12:42:10 -0700

    add stylized display to skills index

--- db/migrate/20180916192647_create_skills.rb
+++ db/migrate/20180916192647_create_skills.rb
@@ -1,4 +1,4 @@
-class AddSkillsTable < ActiveRecord::Migration[5.2]
+class CreateSkills < ActiveRecord::Migration[5.2]
   def change
     create_table :skills do |t|
       t.string :name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_16_191143) do
+ActiveRecord::Schema.define(version: 2018_09_16_192647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
