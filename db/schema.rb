4db031c48b9e7515023a0b9230ee054cee3bcafb
Eric Shull <eshulldesign@gmail.com>
2018-09-16 12:14:03 -0700

    migrate skills table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_16_033237) do
+ActiveRecord::Schema.define(version: 2018_09_16_191143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2018_09_16_033237) do
     t.string "category"
     t.string "img_tag"
     t.string "url"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.string "detail"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
