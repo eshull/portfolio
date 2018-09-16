cad09fbe9d23085e9be636727b488b930b634c04
Eric Shull <eshulldesign@gmail.com>
2018-09-15 20:34:29 -0700

    migration migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_14_182703) do
+ActiveRecord::Schema.define(version: 2018_09_16_033237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2018_09_14_182703) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
+    t.string "image_tag"
+    t.string "url"
   end
 
   create_table "users", force: :cascade do |t|
