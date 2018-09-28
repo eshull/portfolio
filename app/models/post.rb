e1c0727692efc4012e1decd0271fbd6a21438561
Eric Shull <eshulldesign@gmail.com>
2018-09-28 16:49:36 -0700

    add scope method to index for sorting posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+
+  def self.sort_by(sort_value)
+  if sort_value == 'Design'
+    self.sort_design
+  elsif sort_value == 'Development'
+    self.sort_develop
+  else
+    self.all
+  end
+end
+
+scope :sort_design, -> {(
+    select("posts.id, posts.author, posts.title, posts.content, posts.category, posts.img_tag, posts.url")
+    .where(category: "Design")
+    )}
+
+  scope :sort_develop, -> {(
+    select("posts.id, posts.author, posts.title, posts.content, posts.category, posts.img_tag, posts.url")
+    )}
+
 end
